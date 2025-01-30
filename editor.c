d716eab5f772d8b2faf5a0c10f30e7813b8ef8de
zeb33n <zeb.bond@33n.co.uk>
2025-01-30 16:43:33 +0000

    optimisations and refactors

--- editor.c
+++ editor.c
@@ -1,6 +1,6 @@
 #include "editor.h"
 
-static Screen SCREEN = {0, 0, 0, 0, 0, 0, NULL};
+static Screen SCREEN = {0, 0, 0, 0, 0, 0, 0, 0, NULL};
 
 Coord cursor_get_pos_cs() {
   Coord out = {SCREEN.cursor % SCREEN.width_cs,
@@ -10,26 +10,10 @@ Coord cursor_get_pos_cs() {
 
 Coord cursor_get_pos_px() {
   Coord out = cursor_get_pos_cs();
-  out.x *= (SCREEN.font_size / FONT_HW_R);
-  out.y *= (SCREEN.font_size + FONT_VP_PX);
-  out.y += SCREEN.font_size;
+  out.x *= SCREEN.cursor_x_os;
+  out.y *= SCREEN.cursor_y_os;
+  out.y += SCREEN.font_px;
   return out;
-}
-
-void init(int w, int h, int font_size) {
-  int w_chars = w / (font_size / FONT_HW_R);
-  int h_chars = h / (font_size + FONT_VP_PX);
-  if (w_chars * h_chars > LEN_MAX) {
-    return;
-  }
-  SCREEN.width_px = w;
-  SCREEN.height_px = h;
-  SCREEN.width_cs = w_chars;
-  SCREEN.height_cs = h_chars;
-  SCREEN.font_size = font_size;
-  static char text[LEN_MAX];
-  SCREEN.text = text;
-  fill_rect(0, 0, w, h, 0xFF181818);
 }
 
 // TODO perhaps render from cursor will be useful
@@ -39,27 +23,98 @@ void render_screen(void) {
   }
   for (int i = 0; i < SCREEN.height_cs; i++) {
     for (int j = 0; j < SCREEN.width_cs; j++) {
-      write_char(
-          (j * SCREEN.font_size) / 2, i * SCREEN.font_size + SCREEN.font_size,
-          SCREEN.text[j + i * SCREEN.width_cs], 0xEEEEEEEE, SCREEN.font_size);
+      write_char((j * SCREEN.font_px) / 2, i * SCREEN.font_px + SCREEN.font_px,
+                 SCREEN.text[j + i * SCREEN.width_cs], TEXT_COL,
+                 SCREEN.font_px);
     }
   }
+}
+
+void cursor_render_line_from() {}
+
+void cursor_render() {
+  Coord cursor_coords = cursor_get_pos_px();
+  fill_rect(cursor_coords.x, cursor_coords.y - (SCREEN.font_px / 1.2), 4,
+            SCREEN.font_px, TEXT_COL);
+}
+
+void cursor_clear() {
+  Coord cursor_coords = cursor_get_pos_px();
+  fill_rect(cursor_coords.x, cursor_coords.y - (SCREEN.font_px / 1.2), 4,
+            SCREEN.font_px, BGRD_COL);
+}
+
+void init(int w, int h, int font_size) {
+  // Check if size is ok
+  int w_chars = w / (font_size / FONT_HW_R);
+  int h_chars = h / (font_size + FONT_VP_PX);
+  if (w_chars * h_chars > LEN_MAX) {
+    return;
+  }
+
+  // Set basic parameters
+  SCREEN.width_px = w;
+  SCREEN.height_px = h;
+  SCREEN.width_cs = w_chars;
+  SCREEN.height_cs = h_chars;
+
+  // Calibrate font size and spacings
+  SCREEN.font_px = font_size;
+  SCREEN.cursor_x_os = font_size / FONT_HW_R;
+  SCREEN.cursor_y_os = font_size + FONT_VP_PX;
+
+  // allocate and initialise text buffer
+  static char text[LEN_MAX];
+  SCREEN.text = text;
+  for (int i = 0; i < LEN_MAX; i++) {
+    SCREEN.text[i] = 0;
+  }
+
+  // initial rendering
+  fill_rect(0, 0, w, h, BGRD_COL);
+  cursor_render();
 }
 
 void execute_bspace() {
   if (SCREEN.cursor == 0) {
     return;
   }
+  cursor_clear();
   SCREEN.cursor--;
   Coord cursor_coords = cursor_get_pos_px();
-  unwrite_char(cursor_coords.x, cursor_coords.y, 0xFF181818, SCREEN.font_size);
+  unwrite_char(cursor_coords.x, cursor_coords.y, BGRD_COL, SCREEN.font_px);
   SCREEN.text[SCREEN.cursor] = 0;
+  cursor_render();
+}
+
+void execute_left() {
+  if (SCREEN.cursor == 0) {
+    return;
+  }
+  cursor_clear();
+  SCREEN.cursor--;
+  cursor_render();
+}
+
+void execute_right() {
+  if (SCREEN.cursor >= SCREEN.width_cs * SCREEN.height_cs) {
+    return;
+  }
+  cursor_clear();
+  SCREEN.cursor++;
+  cursor_render();
 }
 
 void editor_special_keypress(char c) {
   switch (c) {
     case BSPACE:
       execute_bspace();
+      return;
+    case LEFT:
+      execute_left();
+      return;
+    case RIGHT:
+      execute_right();
       return;
     default:
       return;
@@ -70,10 +125,12 @@ void editor_keypress(char c) {
   if (SCREEN.cursor >= SCREEN.width_cs * SCREEN.height_cs) {
     return;
   }
+  cursor_clear();
   Coord cursor_coords = cursor_get_pos_px();
-  write_char(cursor_coords.x, cursor_coords.y, c, 0xEEEEEEEE, SCREEN.font_size);
+  write_char(cursor_coords.x, cursor_coords.y, c, TEXT_COL, SCREEN.font_px);
   SCREEN.text[SCREEN.cursor] = c;
   SCREEN.cursor++;
+  cursor_render();
 }
 
 void foo(void) {
--- editor.h
+++ editor.h
@@ -12,11 +12,16 @@ void init(int w, int h, int font_size);
 #define LEN_MAX 80 * 100
 #define FONT_HW_R 1.7
 // figure out a better way to calc this
+// maybe figure it out in init ratio of font size
 #define FONT_VP_PX 10 * FONT_HW_R
+#define BGRD_COL 0xFF181818
+#define TEXT_COL 0xEEEEEEEE
 
 enum KeysSpecial {
   ENTER = 13,
+  LEFT = 37,
   UP = 38,
+  RIGHT = 39,
   DOWN = 40,
   BSPACE = 8,
   DELETE = 46,
@@ -28,8 +33,10 @@ typedef struct Screen {
   int height_px;
   int width_cs;
   int height_cs;
-  int font_size;
+  int font_px;
   int cursor;
+  int cursor_x_os;
+  int cursor_y_os;
   char* text;
 } Screen;
 
